638169ee20df5409fadbf6fadbff04773c18a2b7
juliano santos <jot.asa@hotmail.com>
2023-04-11 19:16:47 -0300

    formatação de clientes

--- resources/views/clientform.blade.php
+++ resources/views/clientform.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,37 +8,48 @@
     <title>Sistema Controle</title>
 </head>
 <body>
+<div class="container col-12">
     <form action="{{ route('client-form') }}" method="post">
         @csrf
-        <header>    
+        <header>
             <h4>SISTEMA CONTROLE</h4>
         </header>
         <h5>CADASTRO DE CLIENTES</h5>
-            <div class="frame">
+            <div class="row">
                 <main class="cliente">
-                    <div class="primeira">
+                    <div class="col-12 col-sm-6 col-md-4"> 
                         <label>Nome: </label>
-                        <input type="text" size="50px" name="name">
+                        <input type="text" name="name">
+                    
                         <label>Cpf: <label>
                         <input type="text" name="cpf">
+                   
                         <label>Rg: </label>
                         <input type="text" name="rg">
                     </div>
-                    <div class="segunda">
-                        <label>Rua: </label>
-                        <input type="text" size="50px" name="street">
-                        <label>Número: </label>
-                        <input type="text" name="number">
-                        <label>Bairro: </label>
-                        <input type="text" name="district">
-                    </div>
-                    <div class="terceira">
-                        <label>Cidade: </label>
-                        <input type="text" size="50px" name="city">
+                    <div class="col-12 col-sm-6 col-md-4">    
                         <label>Celular: </label>
                         <input type="text" name="fone">
+                   
                         <label>Celular 2: </label>
                         <input type="text" name="celfone">
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <label>Cep: </label>
+                        <input type="text" name="cep">
+                    
+                        <label>Rua: </label>
+                        <input type="text"  name="street">
+                    
+                        <label>Número: </label>
+                        <input type="text" name="number">
+                        </div>
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <label>Bairro: </label>
+                        <input type="text" name="district">
+                    
+                        <label>Cidade: </label>
+                        <input type="text"  name="city">
                     </div>
                 </main>
             </div>
@@ -47,5 +58,6 @@
             <input type="reset" value="limpar">
         </footer>
     </form>
+</div>
 </body>
 </html>
